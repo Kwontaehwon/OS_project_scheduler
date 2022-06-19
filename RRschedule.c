504633df1b58f9cbb3e00f8dec7ca7633c071190
Kwontaehwon <kygkth2011@gmail.com>
2022-06-19 19:07:41 +0900

    RR shedule.c

--- RRschedule.c
+++ RRschedule.c
@@ -8,9 +8,11 @@ struct Process {
     int bursttime; //파일에서 입력받는값
     int waittime;
     int remaintime;
+    int turnaroundtime;
+    int responsetime;
 };
 
-int input_data(struct Process* p){
+int input_data(struct Process p[]){
     // 텍스트 파일을 읽어 main의 프로세스 구조체 포인터 배열에 저장하는 함수
     FILE *fd = fopen("process.txt", "r");
     char data[255];
@@ -40,27 +42,39 @@ int input_data(struct Process* p){
     return i-1; // i개의 프로세스 (id : 1 ~ i)
 }
 
-void SJFscheduler() {
-    struct Process p[100]; //프로세스 리스트
-    int num_process = input_data(p); // 파일에서 입력받기 입력받은 프로세스 의 수
+void remove_process(struct Process *p, int pos) {//ready배열 대기수 하나 줄이기 
+    p[pos].p_id = 0;  
+    p[pos].arrivetime = 0;
+    p[pos].bursttime = 0;
+    p[pos].remaintime = 0;
+    p[pos].waittime = 0;
+    p[pos].responsetime = 0;
+    p[pos].turnaroundtime = 0;
+}
+
+void clear_process(struct Process *p, int process_num) {//배열 초기화
+    for(int i=0;i<process_num;i++){
+        p[i].waittime = 0;
+        p[i].responsetime = 0;
+        p[i].turnaroundtime = 0;
+    }
+}
+
+void SJFscheduler(struct Process *p, int num_process) {
     int origin_numprocess = num_process;
     int currenttime = 0;
     int excute[1000] = {0,};
-    struct Process ready[100]; //레디배열
-    ready[0].p_id = 0;
-    int readynum = 0; //레디배열에 있는 프로세스 수
-    int processlist[100] = {0,};
-
-    FILE *fd = fopen("pytxt.txt", "w");
-    fprintf(fd,"%d\n",num_process);
-
+    struct Process ready[100]; //ready배열
+    clear_process(p,num_process);
+    int readynum = 0; //ready배열에 있는 프로세스 수
+    int processlist[101] = {0,};
     while(num_process > 0){
-        for(int i=0; i<origin_numprocess; i++){ // 들어온 process를 ready 배열에 넣어준다.
+        for(int i=0;i<origin_numprocess;i++){ // 들어온 process를 ready 배열에 넣어준다.
             if(p[i].arrivetime == currenttime){
                 memcpy(&ready[readynum], &p[i], sizeof(struct Process));
                 readynum++;
-                for(int i=0; i<readynum-1; i++){
-                     for(int j=i+1; j<readynum; j++){
+                for(int i=0;i<readynum-1;i++){ // ready 배열에 추가되면 정렬을 해준다.
+                     for(int j=i+1;j<readynum;j++){
                         if(ready[i].remaintime > ready[j].remaintime){
                             struct Process temp = ready[i];
                             ready[i] = ready[j];
@@ -70,61 +84,98 @@ void SJFscheduler() {
                 }
             }
         }
-        if(readynum > 0) {
+        if(readynum > 0) { // ready배열이 하나이상 차있을때
             if(processlist[ready[0].p_id] == 0){ // 실행했던 프로세스 리스트
                 processlist[ready[0].p_id] = 1;
-                p[ready[0].p_id - 1].waittime = currenttime;
+                p[ready[0].p_id - 1].waittime = currenttime - p[ready[0].p_id - 1].arrivetime;
+                p[ready[0].p_id - 1].responsetime = currenttime;
             }
             excute[currenttime] = ready[0].p_id;
             ready[0].remaintime--;
-            fprintf(fd,"%d %d %d\n",ready[0].p_id, currenttime, 1);
             if(ready[0].remaintime == 0){
+                p[ready[0].p_id - 1].turnaroundtime = currenttime;
                 for(int i=0; i<readynum-1; i++) {
                     memcpy(&ready[i],&ready[i+1],sizeof(struct Process));
                 }
+                remove_process(ready, readynum-1);
                 readynum--;
                 num_process--;
             }
         } else {
             excute[currenttime] = 0;
         }
+
         currenttime++;
     }
 
-    
-    for(int i=0;i<origin_numprocess;i++){
-        printf("p_id %d waiting time : %d\n",p[i].p_id,p[i].waittime);
-    }
+    int idle_time=0;
     for(int i=0;i<currenttime;i++){
-        printf("%d ", excute[i]);
+        if(excute[i] == 0 )idle_time++;
     }
-    
-    printf("Exit : %d\n", currenttime);
+
+    float utilization = (1- ((float)idle_time/(currenttime-1))) * 100;
+
     printf("\n");
-    fclose(fd);
+    float aver_response = 0, aver_turnaround = 0, aver_waiting = 0;
+    printf("\
++-------+--------+----------+-------+\n\
+|PROCESS|RESPONSE|TURNAROUND|WAITING|\n\
+|   ID  |  TIME  |   TIME   | TIME  |\n\
++-------+--------+----------+-------+\n");
+    for(int i = 0; i < origin_numprocess; i++){
+        printf("|  %3d  |", p[i].p_id);
+        printf("  %5d |", p[i].responsetime);
+        printf("    %5d |", p[i].turnaroundtime);
+        printf(" %5d |\n", p[i].waittime);
+
+        aver_response += p[i].responsetime;
+        aver_turnaround += p[i].turnaroundtime;
+        aver_waiting += p[i].waittime;
+    }
+    printf("+-------+--------+----------+-------+\n\n");
+
+    aver_response /= origin_numprocess;
+    aver_turnaround /= origin_numprocess;
+    aver_waiting /= origin_numprocess;
+
+    printf("Total %d Processes\n", origin_numprocess);
+    printf("Total Runtime           : %d\n", currenttime-1);
+    printf("Utilization             : %.3lf%%\n", utilization);
+    printf("Idle Time               : %d\n", idle_time);
+    printf("Average Waiting Time    : %.3f\n", aver_waiting);
+    printf("Average Turnaround Time : %.3f\n", aver_turnaround);
+    printf("Average Response Time   : %.3f\n", aver_response);
+    printf("\n");
+
 }
 
-void RRscheduler() {
-    struct Process p[100]; //프로세스 리스트
-    int num_process = input_data(p); // 파일에서 입력받기 입력받은 프로세스 의 수
+void RRscheduler(struct Process *p, int num_process) {
+    // struct Process p[100]; //프로세스 리스트
+    // int num_process = input_data(&p); // 파일에서 입력받기 입력받은 프로세스 의 수
 
     FILE *fd = fopen("pytxt.txt", "w");
     fprintf(fd,"%d\n",num_process);
 
     int origin_numprocess = num_process;
     int currenttime = 0;
-    int timeslice;
-    printf("timeslice input\n");
-    scanf("%d", &timeslice);
+    int timequantum;
+    printf("timequantum input\n");
+    scanf("%d", &timequantum);
     int excute[1000] = {0,};
-    struct Process ready[100]; //레디배열
-    ready[0].p_id = 0;
-    int readynum = 0; //레디배열에 있는 프로세스 수
+    struct Process ready[100]; //ready배열
+    clear_process(p,num_process);
+    int readynum = 0; //ready배열에 있는 프로세스 수
     int now_excute = 0; // 실행중인지 확인
     int process_excutetime = 0; //각 프로세스에서 실행중인 시간
-    int processlist[100] = {0,}; //프로세스 목록
+    int processlist[101] = {0,}; //프로세스 목록
 
     while(num_process > 0){
+        if(readynum>1){
+            for(int i=1;i<readynum;i++){
+                p[ready[i].p_id - 1].waittime++;
+            }
+        }
+
         for(int i=0;i<origin_numprocess;i++){ // 들어온 process를 ready 배열에 넣어준다.
             if(p[i].arrivetime == currenttime){
                 memcpy(&ready[readynum], &p[i], sizeof(struct Process));
@@ -137,91 +188,126 @@ void RRscheduler() {
                 now_excute = 1; //프로세스 실행중으로 바꾸기
                 if(processlist[ready[0].p_id] == 0){
                     processlist[ready[0].p_id] = 1;
-                    p[ready[0].p_id - 1].waittime = currenttime;
+                    p[ready[0].p_id - 1].responsetime = currenttime;
                 }
             } else {
                 excute[currenttime] = 0;
+                process_excutetime = 0;
             }
         }
-        
+
         if(now_excute == 1){ //실행중인 프로세스 있을때
-            if(processlist[ready[0].p_id] == 0){ // 실행했던 프로세스 리스트
-                processlist[ready[0].p_id] = 1;
-                p[ready[0].p_id - 1].waittime = currenttime;
-            }
             excute[currenttime] = ready[0].p_id; 
             ready[0].remaintime--;
             process_excutetime++;
-            if(ready[0].remaintime == 0){ //레디배열에 첫번째 프로세스의 남은시간이 없을때
-                printf("Process %d (%d, %d)\n",ready[0].p_id, currenttime-process_excutetime+1 , process_excutetime);
-                fprintf(fd,"%d %d %d\n",ready[0].p_id, currenttime-process_excutetime+1 , process_excutetime);
-                if(readynum > 1){ //레디배열에 대기중인 프로세스가 있을때
+            if(ready[0].remaintime == 0){ //ready배열에 첫번째 프로세스의 남은시간이 없을때
+                p[ready[0].p_id - 1].turnaroundtime = currenttime;
+                if(readynum > 1){ //ready배열에 대기중인 프로세스가 있을때
                     for(int i=0;i<readynum-1;i++){
-                    memcpy(&ready[i],&ready[i+1],sizeof(struct Process)); // 레디배열 한칸씩 앞으로 당기기
+                    memcpy(&ready[i],&ready[i+1],sizeof(struct Process)); // ready배열 한칸씩 앞으로 당기기
                     }
-                    ready[readynum-1].p_id = 0;  //레디배열 대기수 하나 줄이기 
-                    ready[readynum-1].arrivetime = 0;
-                    ready[readynum-1].bursttime = 0;
-                    ready[readynum-1].remaintime = 0;
-                    ready[readynum-1].waittime = 0;
-                } else { //레디배열에 대기중인 프로세스가 없을때
-                    ready[0].p_id = 0;  //레디배열 비워주기
-                    ready[0].arrivetime = 0; 
-                    ready[0].bursttime = 0; 
-                    ready[0].remaintime = 0; 
-                    ready[0].waittime = 0; 
+                    remove_process(ready,readynum-1);
+                } else { //ready배열에 대기중인 프로세스가 없을때
+                    remove_process(ready,0);
+                    now_excute = 0;
                 }
                 readynum--;
                 num_process--;
                 process_excutetime = 0;
             }
 
-            if(timeslice == process_excutetime){ // 실행중인 프로세스가 타임슬라이스만큼 실행했을때
-                //printf("Process %d execution finshed at %d\n",ready[0].p_id, currenttime); // Time Quantum안에 끝나지 않고 다음 프로세스로 넘어가는 경우
-                printf("Process %d (%d, %d)\n",ready[0].p_id, currenttime-process_excutetime+1 , process_excutetime);
-                fprintf(fd,"%d %d %d\n",ready[0].p_id, currenttime-process_excutetime+1 , process_excutetime);
-
-                if(readynum > 1){ //레디배열에 대기 중인 프로세스가 있을때
+            if(timequantum == process_excutetime){ // 실행중인 프로세스가 타임슬라이스만큼 실행했을때
+                if(readynum > 1){ //ready배열에 대기 중인 프로세스가 있을때
                     struct Process temp = ready[0];
-                    for(int i=0; i<readynum-1; i++){
-                        ready[i] = ready[i+1]; //레디배열 앞으로 한칸씩 당기기
+                    for(int i=0;i<readynum-1;i++){
+                        ready[i] = ready[i+1]; //ready배열 앞으로 한칸씩 당기기
                     }
-                    ready[readynum-1] = temp; //실행하던 프로세스를 레디배열 맨마지막으로 보내기
+                    ready[readynum-1] = temp; //실행하던 프로세스를 ready배열 맨마지막으로 보내기
                 }
                 process_excutetime = 0;
+                if(processlist[ready[0].p_id] == 0){
+                    processlist[ready[0].p_id] = 1;
+                    p[ready[0].p_id - 1].responsetime = currenttime+1;
+                }
             }
         }
         currenttime++;
     }
 
-    
-    for(int i=0;i<origin_numprocess;i++){
-        printf("p_id %d waiting time : %d\n",p[i].p_id,p[i].waittime);
-    }
+    int idle_time=0;
     for(int i=0;i<currenttime;i++){
-        printf("%d ", excute[i]);
+        if(excute[i] == 0 )idle_time++;
     }
-    
-    printf("Exit : %d\n", currenttime);
-    fprintf(fd,"%d",currenttime);
-    fclose(fd);
+
+    float utilization = (1- ((float)idle_time/(currenttime-1))) * 100;
+
+    printf("\n");
+    float aver_response = 0, aver_turnaround = 0, aver_waiting = 0;
+    printf("\
++-------+--------+----------+-------+\n\
+|PROCESS|RESPONSE|TURNAROUND|WAITING|\n\
+|   ID  |  TIME  |   TIME   | TIME  |\n\
++-------+--------+----------+-------+\n");
+    for(int i = 0; i < origin_numprocess; i++){
+        printf("|  %3d  |", p[i].p_id);
+        printf("  %5d |", p[i].responsetime);
+        printf("    %5d |", p[i].turnaroundtime);
+        printf(" %5d |\n", p[i].waittime);
+
+        aver_response += p[i].responsetime;
+        aver_turnaround += p[i].turnaroundtime;
+        aver_waiting += p[i].waittime;
+    }
+    printf("+-------+--------+----------+-------+\n\n");
+
+    aver_response /= origin_numprocess;
+    aver_turnaround /= origin_numprocess;
+    aver_waiting /= origin_numprocess;
+
+    printf("Total %d Processes\n", origin_numprocess);
+    printf("Total Runtime           : %d\n", currenttime-1);
+    printf("Utilization             : %.3lf%%\n", utilization);
+    printf("Idle Time               : %d\n", idle_time);
+    printf("Average Waiting Time    : %.3f\n", aver_waiting);
+    printf("Average Turnaround Time : %.3f\n", aver_turnaround);
+    printf("Average Response Time   : %.3f\n", aver_response);
     printf("\n");
 }
 
 int main(){
     int input;
-    printf("------- TEAM 4 Schedular -------\n");
-    printf("--------------------------------\n");
-    printf("1. RR scheduler\n");
-    printf("2. SJF scheduler\n");
-    printf("--------------------------------\n");
-    scanf("%d",&input);
-    if(input == 1) {
-        RRscheduler();
-    } else if(input == 2) {
-        SJFscheduler();
-    } else {
-        printf("INPUT 1 or 2\n");
+    int ok=0;
+    struct Process p[100]; //프로세스 리스트
+    int num_process = input_data(p); // 파일에서 입력받기 입력받은 프로세스 의 수
+    printf("\n");
+    float aver_response = 0, aver_turnaround = 0, aver_waiting = 0;
+    printf("\
++-------+--------+----------+\n\
+|    1.RR     |    2.SJF    |\n\
+|  Scheduler  |  Scheduler  |\n\
++-------+--------+----------+\n\
+|PROCESS| ARRIVE |   BURST  |\n\
+|   ID  |  TIME  |   TIME   |\n\
++-------+--------+----------+\n");
+    for(int i = 0; i < num_process; i++){
+        printf("|  %3d  |", p[i].p_id);
+        printf("  %5d |", p[i].arrivetime);
+        printf("    %5d |\n", p[i].bursttime);
+    }
+    printf("+-------+--------+----------+\n\n");
+
+    while(!ok){
+        printf("input : ");
+        scanf("%d",&input);
+        if(input == 1) {
+            RRscheduler(p, num_process);
+            ok = 1;
+        } else if(input == 2) {
+            SJFscheduler(p, num_process);
+            ok = 1;
+        } else {
+            printf("input is 1 or 2\n");
+        }
     }
     return 0;
 }
